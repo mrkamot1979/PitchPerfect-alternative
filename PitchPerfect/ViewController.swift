e212cc5372d74b5e70db1c1072e70e98f54fd124
nenu <nino_roldan@yahoo.com>
2018-06-20 23:37:26 +0900

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Nino Rorudan on 6/20/18.
+//  Copyright © 2018 The NTMC Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func recordAudio(_ sender: Any)
+    {
+        print("Hello World")
+    }
+}
+
