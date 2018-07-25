5d2c99290b6084e72fd8bb247940a2e4a5d6b5d5
nenu <nino_roldan@yahoo.com>
2018-07-25 21:35:58 +0900

    Added tags to buttons and also updated the playSoundForButton function.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -30,7 +30,21 @@ class PlaySoundsViewController: UIViewController {
     enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb }
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("play sound button pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.playing)
     }
    
     @IBAction func stopButtonPressed(_sender: AnyObject) {
